16a46f28916941899a93e0d1770d92635fe7ce40
Sowmiya1905 <144463824+Sowmiya1905@users.noreply.github.com>
2024-07-05 20:41:36 +0530

    Add files via upload

---
+++ Leapyear/javascript/script.js
@@ -0,0 +1,7 @@
+var year=parseInt(prompt("Enter a Leap Year"))
+    if( year % 4 == 0){
+        document.write("It is Leap Year")
+    }
+        else{
+            document.write("It is Not a Leap Year")
+        }
