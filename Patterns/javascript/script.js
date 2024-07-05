ce793a1cdb929be924b0eb3708cce2bd05e3bbdf
Sowmiya1905 <144463824+Sowmiya1905@users.noreply.github.com>
2024-07-05 20:42:38 +0530

    Add files via upload

---
+++ Patterns/javascript/script.js
@@ -0,0 +1,40 @@
+//  var i=1;
+        //  for(i=1;i<=5;i++){
+        //     for(j=1;j<=i;j++){
+        //         document.write("*")
+        //     }
+        //     document.write("<br>")
+        //  }
+
+        // var i=5;
+        // for(i=1;i<=5;i++){
+        //     for(j=5;j>=i;j--){
+        //         document.write("*") 
+        //     }
+        //     document.write("<br>")
+        //  }
+
+        var i=1
+        for(i=1;i<=5;i++){
+            for(j=1;j<=5;j++){
+                document.write("*")
+            }
+            document.write("<br>")
+        }
+
+
+
+        // let string = "";
+    
+        // for (let i = 1; i <= 5; i++) {
+          
+        //   for (let j = 5; j > i; j--) {
+        //     string += " ";
+        //   }
+         
+        //   for (let k = 0; k < i * 2 - 1; k++) {
+        //     string += "*";
+        //   }
+        //   string += "<br>";
+        // }
+        // document.write(`<pre>${string}</pre>`);
